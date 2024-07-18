dd217e3933ec7a3b03d6a34a09ae86ad077cb582
abertierir <abertierir@unal.edu.co>
2024-07-18 12:51:47 -0500

    Add first preact file - User Interaction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+import preact from "@astrojs/preact";
 export default defineConfig({
-    site: "https://portfolio-abertieri.netlify.app/"
+  site: "https://portfolio-abertieri.netlify.app/",
+  integrations: [preact()]
 });
